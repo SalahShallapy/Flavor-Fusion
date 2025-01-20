43eb63c6c431cb31f3ff03d8af77cdf725d8c652
shalapy <salahshalapy999@gmail.com>
2025-01-21 00:01:01 +0200

    Altered Community Page

--- app/community/page.jsx
+++ app/community/page.jsx
@@ -24,7 +24,7 @@ export default function CommunityPage() {
           </li>
           <li>
             <Image src={communityIcon} alt="A crowd of people, cooking" />
-            <p>Find alot of new friends & like-minded peopldsdsfsde</p>
+            <p>Find alot of new friends & like-minded people</p>
           </li>
           <li>
             <Image
